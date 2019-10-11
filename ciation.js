82ba02cd5f3043012e48a337bbbd3ebbc1ec128d
comline33 <microrepair33@gmail.com>
2019-10-11 11:10:01 +0200

    ajout fichier citation.js

---
+++ ciation.js
@@ -0,0 +1,8 @@
+const debut_citation_amour = ["Je ne peux imaginer la vie Sans ta présence auprès de moi" ,"Je ne peux imaginer la vie,Sans ton sourire pour illuminer mes jours"
+];
+
+const milieu_citation_amour = ["Sans la tendresse de tes paroles Et la douceur de tes doigts",
+];
+
+const fin_citation_amour = ["Sans la tendresse de tes paroles Et la douceur de tes doigts"
+];
